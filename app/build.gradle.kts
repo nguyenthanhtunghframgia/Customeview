bb2edb69c937fc0a396afa457ab523ed81ca8cb7
nguyenthanhtunghframgia <nguyen.thanh.tungh@framgia.com>
2018-12-19 10:17:52 +0700

    develop

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        applicationId = "com.example.framgianguyenthanhtungh.customeview"
+        minSdkVersion(15)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.11")
+    implementation("androidx.appcompat:appcompat:1.1.0-alpha01")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0-alpha2")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+}
