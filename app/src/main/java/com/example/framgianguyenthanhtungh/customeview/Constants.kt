dc6b431e1264dd06fff8f680ac4f9941ef9e3eba
nguyenthanhtunghframgia <nguyen.thanh.tungh@framgia.com>
2019-01-02 09:35:51 +0700

    Halo

---
+++ app/src/main/java/com/example/framgianguyenthanhtungh/customeview/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.framgianguyenthanhtungh.customeview
+
+const val PROPERTY_RADIUS = "RADIUS"
+const val PROPERTY_ROTATE = "ROTATE"
+const val PROPERTY_X = "X"
+const val PROPERTY_Y = "Y"
+const val PROPERTY_ALPHA = "ALPHA"
--- app/src/main/java/com/example/framgianguyenthanhtungh/customeview/MainActivity.kt
+++ app/src/main/java/com/example/framgianguyenthanhtungh/customeview/MainActivity.kt
@@ -5,14 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var customDiagram : CustomDiagram
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        customDiagram = findViewById(R.id.custom)
-        custom.setOnClickListener {
-            customDiagram.changeColor()
-        }
+        custom.anim()
     }
 }
